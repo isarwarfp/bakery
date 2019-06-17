270aad428ff546a744bbd5b54ffc4bd1bae5dd0f
Imran Sarwar <imranbutt.java@gmail.com>
2019-06-17 10:07:00 +0200

    Fixing Issue: Unavailable combination
    For unavailable combination we need to display some message.

--- src/main/java/com/hexad/interview/model/PurchaseResponse.java
+++ src/main/java/com/hexad/interview/model/PurchaseResponse.java
@@ -5,10 +5,20 @@ import java.util.List;
 import java.util.Map;
 
 public class PurchaseResponse {
+    private static final String ERROR_MSG = "For this number we don't have any combination of packs.";
     private Map<Integer, List<Pack>> packMap = new HashMap<>();
     private BakeryProduct product;
     private int quantity;
     private double total;
+    private boolean isFound;
+
+    public boolean isFound() { 
+        return isFound; 
+    }
+
+    public void setFound(boolean found) { 
+        isFound = found; 
+    }
 
     public BakeryProduct getProduct() {
         return product;
@@ -50,6 +60,8 @@ public class PurchaseResponse {
     private String toStringImpl() {
         StringBuilder sb = new StringBuilder();
         sb.append(quantity).append(" ").append(product.getCode()).append(" $").append(total);
+        
+        if(!isFound()) return sb.append("\n\t\t").append(ERROR_MSG).toString();
 
         for(Map.Entry<Integer, List<Pack>> packEntry: packMap.entrySet()) {
             sb.append("\n\t\t");
